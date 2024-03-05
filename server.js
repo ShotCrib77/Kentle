4a8180c13889b855c1945c66a434e0b26ba62463
ShotCrib77 <Wilmer.Safsten@gmail.com>
2024-03-05 22:22:47 +0100

    IT WORK.

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
+const cors = require("cors");
 
 const app = express();
 const port = 3000;
+
+app.use(cors({
+  origin: ["http://localhost:3000", "http://127.0.0.1:5500"]  // Adjusted origins for clarity
+}));
 
 const folderPath = 'music'; // Replace with the actual folder path
 
@@ -14,7 +19,7 @@ app.get('/getRandomFile', (req, res) => {
     } else {
       const randomFile = files[Math.floor(Math.random() * files.length)];
       const filePath = path.join(folderPath, randomFile);
-      res.send(filePath); // Send the path of the randomly selected file
+      res.send(filePath);
     }
   });
 });
