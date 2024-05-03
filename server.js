44a2e04c4ae41d74e768005094e51b35a9b30523
Wilmer Säfsten <Wilmer.Safsten@gmail.com>
2024-05-03 17:43:43 +0200

    Start of album cover change

--- server.js
+++ server.js
@@ -1,25 +1,25 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const cors = require("cors");
+const cors = require("cors"); // Krävs för att kunna använda Localhost som server (annars anses den som opålitlig)
 
 const app = express();
 const port = 3000;
 
 app.use(cors({
-  origin: ["http://localhost:3000", "http://127.0.0.1:5500"]  // Adjusted origins for clarity
+  origin: ["http://localhost:3000", "http://127.0.0.1:5500"] 
 }));
 
-const folderPath = 'music';
+const folderPath = "music";
 
 app.get('/getRandomFile', (req, res) => {
   fs.readdir(folderPath, (err, files) => {
     if (err) {
-      res.status(500).send('Error reading folder');
+      res.status(500).send("Error reading folder");
     } else {
       const randomFile = files[Math.floor(Math.random() * files.length)];
       const filePath = path.join(folderPath, randomFile);
-      res.send(filePath);
+      res.send(filePath); // Svarar med att skicka en string med den slumpmässigt genererade filens path.
     }
   });
 });
